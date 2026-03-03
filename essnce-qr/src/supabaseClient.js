fcd380fde7c80a2d70f44037ef6b8e3b0a8b9e53
marcogutierrezz <marcogt2003@gmail.com>
2026-03-03 16:49:55 -0600

    add env variables

--- essnce-qr/src/supabaseClient.js
+++ essnce-qr/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ydgykykvngciclkwmsou.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkZ3lreWt2bmdjaWNsa3dtc291Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NzE4NTksImV4cCI6MjA4ODE0Nzg1OX0.x4lm2_bYzTp6WP01aXS2trDD8RfTCjP8EHseWF08_qU'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
