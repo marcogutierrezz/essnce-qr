945be0ab8fc38115013828e79a0d823d416000c1
marcogutierrezz <marcogt2003@gmail.com>
2026-03-03 23:05:34 -0600

    attach qr image to email

--- api/send-ticket.js
+++ api/send-ticket.js
@@ -14,7 +14,12 @@ export default async function handler(req, res) {
     try {
 
         const url = `https://essnce-qr.vercel.app/validate/${code}`
+
+        // Generar QR en base64
         const qrImage = await QRCode.toDataURL(url)
+
+        // Quitar el prefijo "data:image/png;base64,"
+        const base64Data = qrImage.split(",")[1]
 
         await resend.emails.send({
             from: 'Essnce <onboarding@resend.dev>',
@@ -23,14 +28,21 @@ export default async function handler(req, res) {
             html: `
         <h1>Hola ${name}</h1>
         <p>Aquí está tu entrada oficial para Essnce.</p>
-        <img src="${qrImage}" />
         <p>No compartas este código.</p>
-      `
+      `,
+            attachments: [
+                {
+                    filename: `entrada-${code}.png`,
+                    content: base64Data,
+                    encoding: "base64"
+                }
+            ]
         })
 
         return res.status(200).json({ success: true })
 
     } catch (error) {
+        console.error(error)
         return res.status(500).json({ error: error.message })
     }
 }
