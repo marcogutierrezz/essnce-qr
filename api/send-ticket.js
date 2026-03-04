8949549877fbf578ed0ceaaa6d757e2217522d31
marcogutierrezz <marcogt2003@gmail.com>
2026-03-04 10:31:59 -0600

    final qr size adjustment

--- api/send-ticket.js
+++ api/send-ticket.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
         const qrSize = 440
 
         const x = (template.width / 2) - (qrSize / 2)
-        const y = 285
+        const y = 295
 
         ctx.drawImage(qr, x, y, qrSize, qrSize)
 
