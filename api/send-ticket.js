37609152a0a4c030cfce163245ab6b6d4656588d
marcogutierrezz <marcogt2003@gmail.com>
2026-03-04 10:08:06 -0600

    generate ticket with qr overlay image

--- api/send-ticket.js
+++ api/send-ticket.js
@@ -1,4 +1,3 @@
-import QRCode from "qrcode"
 import { Resend } from "resend"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
@@ -9,50 +8,24 @@ export default async function handler(req, res) {
 
         const { email, code } = req.body
 
-        const qrUrl = await QRCode.toDataURL(
-            `https://essnce-qr.vercel.app/validate/${code}`
+        const qr = encodeURIComponent(
+            `https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=https://essnce-qr.vercel.app/validate/${code}`
         )
 
+        const ticketImage = `https://images.weserv.nl/?url=essnce-qr.vercel.app/ticket-template.jpg&overlay=${qr}&overlay_x=330&overlay_y=230&overlay_w=200&overlay_h=200`
+
         await resend.emails.send({
-
             from: "Essnce <onboarding@resend.dev>",
-
             to: email,
-
             subject: "Tu entrada para Essnce",
-
             html: `
-
-<div style="background:#000;padding:40px;text-align:center">
-
-<img 
-src="https://essnce-qr.vercel.app/ticket-template.jpg"
-width="420"
-style="display:block;margin:auto;border-radius:10px"
-/>
-
-<div style="margin-top:-420px">
-
-<img
-src="${qrUrl}"
-width="180"
-style="
-background:white;
-padding:10px;
-border-radius:12px;
-"
-/>
-
-</div>
-
-<p style="margin-top:40px;color:white">
-Presenta este código al ingresar
-</p>
-
-</div>
-
-`
-
+      <div style="background:black;padding:40px;text-align:center">
+        <img src="${ticketImage}" width="420" style="border-radius:10px"/>
+        <p style="color:white;margin-top:20px">
+        Presenta este QR en la entrada
+        </p>
+      </div>
+      `
         })
 
         res.status(200).json({ success: true })
