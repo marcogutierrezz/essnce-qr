b3c9ef66aeeee0cc3a07e4b52e1946b858329a8c
marcogutierrezz <marcogt2003@gmail.com>
2026-03-04 22:24:28 -0600

    fix: correct ticket resend after email update in admin edit

--- api/send-ticket.js
+++ api/send-ticket.js
@@ -62,15 +62,6 @@ export default async function handler(req, res) {
             ]
         })
 
-        /* REGISTRO DE ENVIO */
-
-        await supabase
-            .from("ticket_email_logs")
-            .insert({
-                ticket_code: code,
-                sent_to: email
-            })
-
         res.status(200).json({ success: true })
 
     } catch (err) {
