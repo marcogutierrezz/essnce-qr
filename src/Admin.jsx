0d5dc809c0abf2126d8bded3361aca74629aa7be
marcogutierrezz <marcogt2003@gmail.com>
2026-03-05 01:26:19 -0600

    fix: correct ticket resend after email update in admin edit

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -245,7 +245,7 @@ function Admin() {
 
         const qrSize = 220
         const x = (540 / 2) - (qrSize / 2)
-        const y = 150
+        const y = 165
 
         pdf.addImage(qrData, "PNG", x, y, qrSize, qrSize)
 
